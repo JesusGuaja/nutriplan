33e833f766fecf3a94f743346343ca483eea995e
JesusGuaja <jesusguajardo080@gmail.com>
2023-07-03 11:23:56 -0300

    creamos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shares/navbar/navbar.component';
+import { FooterComponent } from './shares/footer/footer.component';
+import { LoginComponent } from './shares/login/login.component';
+import { RegisterComponent } from './shares/register/register.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
