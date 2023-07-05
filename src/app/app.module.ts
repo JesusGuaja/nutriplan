e0f6f51fed4e22cb3d0a0b7bea8d958ddd3e8e81
JesusGuaja <jesusguajardo080@gmail.com>
2023-07-05 11:27:27 -0300

    Terminamos de crear la estructura e incoorporamos bootstrap y primeng al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './shares/navbar/navbar.component';
-import { FooterComponent } from './shares/footer/footer.component';
-import { LoginComponent } from './shares/login/login.component';
-import { RegisterComponent } from './shares/register/register.component';
-import { InicioComponent } from './components/inicio/inicio.component';
+import { ButtonComponent } from './shared/components/button/button.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    LoginComponent,
-    RegisterComponent,
-    InicioComponent
+    ButtonComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/button/button.component.spec.ts
+++ src/app/shared/components/button/button.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InicioComponent } from './inicio.component';
+import { ButtonComponent } from './button.component';
 
-describe('InicioComponent', () => {
-  let component: InicioComponent;
-  let fixture: ComponentFixture<InicioComponent>;
+describe('ButtonComponent', () => {
+  let component: ButtonComponent;
+  let fixture: ComponentFixture<ButtonComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [InicioComponent]
+      declarations: [ButtonComponent]
     });
-    fixture = TestBed.createComponent(InicioComponent);
+    fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
