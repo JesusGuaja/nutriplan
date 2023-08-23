10cf1be0ec544df845d8ad8b8160de0a55f31d87
juanforman <juanchiforman32@gmail.com>
2023-08-23 11:47:47 -0300

    vinculamos la base de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { ButtonComponent } from './shared/components/button/button.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { environment } from 'src/environments/environment';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFireStorageModule} from '@angular/fire/compat/storage';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +23,11 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     RouterModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBoOJkepPkloR-ZlYOYSvlFci9UyfvZ1d8",
+        authDomain: "nutri-plan-65687.firebaseapp.com",
+        projectId: "nutri-plan-65687",
+        storageBucket: "nutri-plan-65687.appspot.com",
+        messagingSenderId: "822762829178",
+        appId: "1:822762829178:web:f639d2f143aa622a3e52c3",
+        measurementId: "G-P6GPS52ST7"
+      }
+}
+
