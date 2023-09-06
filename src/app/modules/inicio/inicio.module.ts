91d9fb12206929fefba1c5044c93f4ec9e22c97e
valentinaortiz1 <valenn.ortiz02@gmail.com>
2023-09-06 09:31:14 -0300

    creacion de componentes

--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InicioComponent,
+    CarruselComponent
+  ],
   imports: [
     CommonModule,
     InicioRoutingModule
