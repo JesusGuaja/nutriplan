6ac0ae76991c60a77c5701dbe3cd34942c190892
valentinaortiz1 <valenn.ortiz02@gmail.com>
2023-09-06 08:23:00 -0300

    comenzamos con el carrusel

---
+++ src/app/modules/inicio/components/carrusel/carrusel.component.ts
@@ -0,0 +1,42 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+  selector: 'app-carrusel',
+  templateUrl: './carrusel.component.html',
+  styleUrls: ['./carrusel.component.css']
+})
+export class CarruselComponent {
+	images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+	paused = false;
+	unpauseOnArrow = false;
+	pauseOnIndicator = false;
+	pauseOnHover = true;
+	pauseOnFocus = true;
+
+	@ViewChild('carousel', { static: true }) carousel: NgbCarousel;
+
+	togglePaused() {
+		if (this.paused) {
+			this.carousel.cycle();
+		} else {
+			this.carousel.pause();
+		}
+		this.paused = !this.paused;
+	}
+
+	onSlide(slideEvent: NgbSlideEvent) {
+		if (
+			this.unpauseOnArrow &&
+			slideEvent.paused &&
+			(slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
+		) {
+			this.togglePaused();
+		}
+		if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+			this.togglePaused();
+		}
+	}
+}
--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -3,18 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
 import { InicioComponent } from './pages/inicio.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {
+  NgbCarousel,
+  NgbCarouselModule,
+} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    InicioComponent
-  ],
+  declarations: [InicioComponent, CarruselComponent],
   imports: [
     CommonModule,
-    InicioRoutingModule
+    InicioRoutingModule,
+    SharedModule,
+    NgFor,
+    FormsModule,
+    NgbCarousel,NgbCarouselModule,  
+
   ],
-  exports: [
-    InicioComponent
-  ]
+  exports: [InicioComponent],
 })
-export class InicioModule { }
+export class InicioModule {}
