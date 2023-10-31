0c4de8dc12e56beb78a78f1d4f5c9a180c086928
JesusGuaja <jesusguajardo080@gmail.com>
2023-10-31 09:03:53 -0300

    creamos la funcion para crear una receta

--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Receta } from 'src/app/models/receta';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
+import { async } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  private recetasColeccion: AngularFirestoreCollection<Receta>
+  
+  constructor(private database: AngularFirestore) { 
+    this.recetasColeccion = database.collection('productos')
+  }
+
+  //funcion para crear la receta
+  crearReceta(receta: Receta){
+    return new Promise(async(resolve, reject) =>{
+      try{
+        const id = this.database.createId();
+        receta.idProducto = id;
+
+        const resultado = await this.recetasColeccion.doc(id).set(receta);
+
+        resolve(resultado);
+
+      }catch (error){
+        reject(error)
+      }
+    })
+  }
+
+  obtenerReceta(){
+    return this.recetasColeccion.snapshotChanges().pipe(map(action => action.map(a => a.payload.doc.data())))
+  }
+
+  //funcion para editar la receta
+  
 }
