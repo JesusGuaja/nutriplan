b78941522ed350cd6bf29334d0697b8df3967cce
JesusGuaja <jesusguajardo080@gmail.com>
2023-10-31 09:22:43 -0300

    llamamos al servicioCrud y le enviamos la nueva receta apartir de la funcion creada agregarReceta

--- src/app/modules/admin/components/table/table.component.ts
+++ src/app/modules/admin/components/table/table.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Receta } from 'src/app/models/receta';
+import { CrudService } from '../../services/crud.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
@@ -7,4 +10,51 @@ import { Component } from '@angular/core';
 })
 export class TableComponent {
 
+  coleccionReceta: Receta [] = [];
+
+  recetaSeleccionada!: Receta;
+
+  modalVisibleReceta: boolean = false;
+
+  receta = new FormGroup({
+    nombre: new FormControl('',Validators.required),
+    imagen: new FormControl('',Validators.required),
+    alt: new FormControl('',Validators.required),
+    descripcion: new FormControl('',Validators.required),
+    categoria: new FormControl('',Validators.required)
+  })
+
+  constructor(
+    public servicioCrud: CrudService
+  ){}
+
+  ngOnInit(): void{
+    this.servicioCrud.obtenerReceta().subscribe(receta => {
+      this.coleccionReceta = receta
+    })
+  }
+
+  async agregarReceta(){
+    if (this.receta.valid){
+      let nuevaReceta: Receta = {
+        idProducto: '',
+        nombre: this.receta.value.nombre!,
+        imagen: this.receta.value.imagen!,
+        alt: this.receta.value.alt!,
+        descripcion: this.receta.value.descripcion!,
+        categoria: this.receta.value.categoria!
+      };
+
+      // enviamos la nueva receta
+      await this.servicioCrud.crearReceta(nuevaReceta)
+      .then(receta => {
+        alert("Ha agregado una nueva receta con exito!");
+      })
+      .catch(error => {
+        alert("Hubo un error al cargar la nueva receta! \n"+error)
+      })
+    }
+  }
+
+  // editar receta
 }
