21abe1abb9edfae5b774271240f9d171be1cc4fe
abril <abrilvalenzuelaa12@gmail.com>
2023-11-06 10:37:15 -0300

    creamos interface usuario

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,6 +5,15 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
 
 
 @NgModule({
@@ -14,7 +23,15 @@ import { SharedModule } from 'src/app/shared/shared.module';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule  ],
+    AuthRoutingModule ,
+     MatSelectModule,
+     MatInputModule,
+     MatFormFieldModule,
+     FormsModule,
+     MatButtonModule,
+     MatIconModule
+
+  ],
   exports: [
     LoginComponent,
     RegisterComponent
