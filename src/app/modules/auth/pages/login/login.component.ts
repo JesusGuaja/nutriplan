bb84b46c907e9651492c8a42a38870aa5729d78d
abril <abrilvalenzuelaa12@gmail.com>
2023-11-06 11:01:10 -0300

    añadimos funcion salir al login

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -33,9 +33,27 @@ export class LoginComponent {
       email: this.usuarios.email,
       contrasena: this.usuarios.contrasena
     }
+
+    const res = await this.servicioAuth.iniciarSesion(credenciales.email, credenciales.contrasena)
+    .then(res => {
+      alert("Se ha logueado con exito");
+
+      this.router.navigate(['/inicio']);
+    })
+    .catch(error => {
+      alert("Hubo un error al inicar sesion" + error);
+    })
   }
 
+  //funcion para Cerrar sesion
+  async salir(){
+    const res = await this.servicioAuth.cerrarSesion()
+    .then(res => {
+      alert("Ha cerrado sesion con exito");
+      console.log(res);
 
-
+      this.router.navigate(['/inicio']);
+    })
+  }
 }
 
