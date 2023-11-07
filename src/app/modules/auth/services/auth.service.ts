b478ca8e10faac7c2e362ab4b8f6867603da85b1
valentinaortiz1 <valenn.ortiz02@gmail.com>
2023-11-07 10:08:23 -0300

    añadimos funciones al servicio auth

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+// Servicio de Autentificación de Firebase
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  // referenciamos Auth de Firebase
+constructor(public auth: AngularFireAuth) { }
+
+// FUNCIÓN PARA LOGIN
+iniciarSesion(email: string, contrasena: string){
+// valida email y contraseña de la BD
+return this.auth.signInWithEmailAndPassword(email, contrasena);
 }
+
+// FUNCIÓN PARA REGISTER
+registrar(email: string, contrasena: string){
+// retorna nuevo valor de nombre y contrasena
+return this.auth.createUserWithEmailAndPassword(email,contrasena);
+}
+
+// FUNCIÓN PARA TOMAR UID
+async getUid(){
+// nos genera una promesa, y const user la captura
+const user = await this.auth.currentUser;
+
+if(user == null){
+return null;
+}else{
+return user.uid;
+}
+}
+
+cerrarSesion(){
+// devuelve una promesa vacía
+return this.auth.signOut();
+}
+}
+
+
+
+
+
+
