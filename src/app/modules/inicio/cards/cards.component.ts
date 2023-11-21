7b8f6e508c764bbd7d30e12ff7faafd1cc471bb8
valentinaortiz1 <valenn.ortiz02@gmail.com>
2023-11-21 09:26:14 -0300

    creamos coleccion receta

--- src/app/modules/inicio/cards/cards.component.ts
+++ src/app/modules/inicio/cards/cards.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Receta } from 'src/app/models/receta';
 import { CrudService } from '../../admin/services/crud.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cards',
@@ -8,7 +10,27 @@ import { CrudService } from '../../admin/services/crud.service';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent {
+  coleccionRecetas : Receta [] = [];
+  recetaSeleccionada! : Receta;
+
+  receta = new FormGroup({
+    nombre: new FormControl('',Validators.required),
+    imagen: new FormControl('',Validators.required),
+    alt: new FormControl('',Validators.required),
+    descripcion: new FormControl('',Validators.required),
+    categoria: new FormControl('',Validators.required)
+  })
+
   constructor (private servicioCrud: CrudService){
-    
+
   }
+
+  // ngOnInit(): void{
+   
+  //    this.servicioCrud.obtenerReceta().subscribe(receta => {
+  //     this.coleccionRecetas = receta;
+  //   })
+  // }
+
+
 }
