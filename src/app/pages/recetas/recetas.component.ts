a0ff45aa612d40b12eb47cf7bee4632232a9c843
JesusGuaja <jesusguajardo080@gmail.com>
2023-12-01 16:29:26 -0300

    borrado de standalone de recetas.ts

--- src/app/pages/recetas/recetas.component.ts
+++ src/app/pages/recetas/recetas.component.ts
@@ -1,25 +1,11 @@
 import { Component } from '@angular/core';
 import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { SharedModule } from 'src/app/shared/shared.module';
+
 
 @Component({
   selector: 'app-recetas',
   templateUrl: './recetas.component.html',
-  styleUrls: ['./recetas.component.css'],
-  standalone: true,
-  imports: [
-    MatCheckboxModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatInputModule,
-    SharedModule
-  ],
+  styleUrls: ['./recetas.component.css']
 })
 export class RecetasComponent {
   disableSelect = new FormControl(false);
