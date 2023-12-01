cfc2bf86856e60396a8679ba590cb00b240d3b27
JesusGuaja <jesusguajardo080@gmail.com>
2023-12-01 16:27:27 -0300

    borrado de importaciones innecesarias en inicio.module

--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { InicioRoutingModule } from './inicio-routing.module';
-
 import { InicioComponent } from './pages/inicio/inicio.component';
-
-
 import { SharedModule } from 'src/app/shared/shared.module';
-
-import { NgFor } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { CardsComponent } from './cards/cards.component';
-import { CrudService } from '../admin/services/crud.service';
 
 @NgModule({
 
