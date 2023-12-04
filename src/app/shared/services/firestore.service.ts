36a6df5d20060aa40b584fc31d2f4d3c286c18a5
JesusGuaja <jesusguajardo080@gmail.com>
2023-12-04 16:39:51 -0300

    implementacion del metodo obternerRol en firestore

--- src/app/shared/services/firestore.service.ts
+++ src/app/shared/services/firestore.service.ts
@@ -8,30 +8,35 @@ import { Usuario } from 'src/app/models/usuario';
 })
 export class FirestoreService {
   private usuariosCollection: AngularFirestoreCollection
-// dentro de los parametros la BD
-constructor(private database: AngularFirestore) {
-// refenciamos colección de la BD
-this.usuariosCollection = this.database.collection('usuarios')
+  // dentro de los parametros la BD
+  constructor(private database: AngularFirestore) {
+    // refenciamos colección de la BD
+    this.usuariosCollection = this.database.collection('usuarios')
+  }
+
+  // Método para obtener el rol de un usuario por su UID
+  obtenerRol(uid: string) {
+    return this.database.collection('usuarios').doc(uid).valueChanges();
+  }
+
+  agregarUsuario(usuario: Usuario, id: string) {
+    // RESOLVE: promesa resulta -> similar al then
+    // REJECT: promesa rechazada -> similar al catch
+    return new Promise(async (resolve, reject) => {
+      try {
+        usuario.uid = id;
+
+        const resultado = await this.usuariosCollection.doc(id).set(usuario)
+        // muestra el resultado sin problema
+        resolve(resultado)
+      } catch (error) {
+        // en caso de que ocurra un error
+        reject(error)
+      }
+    })
+  }
 }
 
-agregarUsuario(usuario: Usuario, id: string) {
-// RESOLVE: promesa resulta -> similar al then
-// REJECT: promesa rechazada -> similar al catch
-return new Promise(async (resolve, reject) => {
-try {
-usuario.uid = id;
-
-const resultado = await this.usuariosCollection.doc(id).set(usuario)
-// muestra el resultado sin problema
-resolve(resultado)
-}catch(error){
-// en caso de que ocurra un error
-reject(error)
-}
-})
-}
-}
-  
 
 
 
