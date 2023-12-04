62bffe9ff1151b674079e63607a8e1360cde59fe
JesusGuaja <jesusguajardo080@gmail.com>
2023-12-04 16:46:48 -0300

    agregado de cerrar sesion

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  constructor(private auth : AuthService, public router : Router){}
+  cerrar(){
+    this.auth.cerrarSesion();
+    this.router.navigate(['/inicio'])
+  }
 
 }
