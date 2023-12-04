473fab27d03f2f431870b023e08ae11441178a56
JesusGuaja <jesusguajardo080@gmail.com>
2023-12-04 16:43:52 -0300

    modificacion de authservice

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,19 +1,36 @@
+// auth.service.ts
 import { Injectable } from '@angular/core';
-// Servicio de Autentificación de Firebase
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Usuario } from 'src/app/models/usuario';
+import { FirestoreService } from 'src/app/shared/services/firestore.service';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  // referenciamos Auth de Firebase
-  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+  usuario$: Observable<Usuario | null>;
 
-  // FUNCIÓN PARA LOGIN
-  iniciarSesion(email: string, contrasena: string) {
-    // valida email y contraseña de la BD
-    return this.afAuth.signInWithEmailAndPassword(email, contrasena);
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private firestore: FirestoreService // Asegúrate de inyectar tu servicio Firestore
+  ) {
+    this.usuario$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          // Aquí deberías obtener el rol del usuario de Firestore
+          // Asegúrate de que el método obtenerRol devuelva un Observable<Usuario>
+          // El método obtenerRol debe manejar el caso de que el usuario no tenga un rol y devolver null en ese caso
+          return this.firestore.obtenerRol(user.uid) as Observable<Usuario | null>;
+        } else {
+          return of(null);
+        }
+      })
+    );
   }
 
   // FUNCIÓN PARA REGISTER
@@ -36,8 +53,39 @@ export class AuthService {
     }
   }
 
+  // Método para iniciar sesión y obtener el token
+  iniciarSesion(email: string, contrasena: string) {
+    return this.afAuth.signInWithEmailAndPassword(email, contrasena)
+      .then((result) => {
+        if (result.user) {
+          // Si result.user no es null, procede a obtener el token
+          return result.user.getIdToken().then((token) => {
+            localStorage.setItem('token', token);
+            // Puedes establecer una fecha de expiración para el token si lo deseas
+            // localStorage.setItem('expira', (new Date().getTime() + (result.user.stsTokenManager.expirationTime)).toString());
+          });
+        } else {
+          // Si result.user es null, maneja el caso como consideres necesario
+          // Por ejemplo, podrías lanzar un error o redirigir al usuario a la página de inicio de sesión
+          throw new Error('No se pudo obtener la información del usuario.');
+        }
+      });
+  }
+
+  // Método para cerrar sesión y eliminar el token
   cerrarSesion() {
-    // devuelve una promesa vacía
-    return this.afAuth.signOut();
+    return this.afAuth.signOut().then(() => {
+      // Eliminar el token del almacenamiento local
+      localStorage.removeItem('token');
+      // Si has establecido una fecha de expiración, también deberías eliminarla
+      // localStorage.removeItem('expira');
+    });
+  }
+
+  // Método para verificar si el token existe y es válido
+  estaLogueado(): boolean {
+    const token = localStorage.getItem('token');
+    // Aquí podrías agregar una verificación adicional para el token, como su fecha de expiración
+    return token != null;
   }
 }
